7c3a5fc8eac8fda35d1b92a17ca79c5b0d5cd948
charles <charles.daniel@relovate.com>
2017-01-01 19:25:56 -0500

    Add project files.

---
+++ tSRclient/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tSRclient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
